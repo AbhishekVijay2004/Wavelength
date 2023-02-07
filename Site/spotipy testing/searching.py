23d346155ad35902d0bc6ca52c1763acf641b681
r01479mo <matthew.ofarrelly@manchester.ac.uk>
2023-02-07 12:11:41 +0000

    added comments to searching example

--- Site/spotipy testing/searching.py
+++ Site/spotipy testing/searching.py
@@ -2,18 +2,28 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 
+
+"""scope is what the api is able to access about a user, list of scopes on 
+https://developer.spotify.com/documentation/general/guides/authorization/scopes/"""
 scope = 'user-top-read user-library-modify'
+#client and secret ids passed to api
+#ids of the website not of the individual user
 client_id = 'bd9c275a853c493ba51fdafefcb08578'
 client_secret = 'ddf20a988de843b296a31d2e20e59d9d'
+#kind of nothing website for now
+#where it goes to if the spotify log in doesn't call
+#should be our actual website when done
 redirect_uri = 'http://localhost:8888/callback'
 
+#create a spotify object allowing us to access the apii
+# has the authorisation code flow authetification method
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=client_id, client_secret=client_secret,  redirect_uri=redirect_uri, scope=scope))
 
 
 
 def get_artist():
     artist_in = input('Search for an artist: ')
-
+    #return the first artist found based no the search query
     artist = sp.search(artist_in, type='artist', limit=1)  
     artist = artist['artists']['items'][0]
     id = artist['id']
@@ -24,6 +34,7 @@ def get_artist():
 
 def get_song():
     song_in = input('Search for a song: ')
+    #return first song based on query, specified to gb market
     song = sp.search(song_in, type='track', limit=1, market='GB')
     # print(song['tracks']['items'][0])
     song = song['tracks']['items'][0]
@@ -38,6 +49,7 @@ def get_song():
 
 def get_album():
     album_in = input('Search for an album: ')
+    #return first album found in gb market for the search query
     album = sp.search(album_in, type='album', limit=1, market='GB')
     album = album['albums']['items'][0]
     id = album['id']
