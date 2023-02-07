778d58acf3ac7238e00ac66d6e8667c272124496
Zak <zak.mitchell@student.manchester.ac.uk>
2023-02-07 11:36:45 +0000

    Dark/Light mode switch optimisation.

--- Site/js/light-dark-switch.js
+++ Site/js/light-dark-switch.js
@@ -1,26 +1,27 @@
-/*
-         Colour           Dark Mode Light Mode
- --background-colour       #1E1E1E   #E9EAEA
- --background-highlight-1  #333333   #
- --background-highlight-2  #474747
- --primary-colour
- --primary-highlight-1
- --primary-highlight-2
-
-*/
-
 var darkMode = true;
 
-var colourDict = {
-  
+var darkModeColourDict = {
+  "--background-colour"      : "#1E1E1E",
+  "--background-highlight-1" : "#333333",
+  "--background-highlight-2" : "#474747",
+  "--primary-colour"         : "#B64CFF",
+  "--primary-highlight-1"    : "#CE85FF",
+  "--primary-highlight-2"    : "#DEADFF"
+}
+
+var lightModeColourDict = {
+  "--background-colour"      : "#E9EAEA",
+  "--background-highlight-1" : "#D5D7D7",
+  "--background-highlight-2" : "#C1C3C3",
+  "--primary-colour"         : "#B64CFF",
+  "--primary-highlight-1"    : "#AD33FF",
+  "--primary-highlight-2"    : "#9D0AFF"
 }
 
 function SwitchMode(){
   darkMode = !darkMode;
-  if (darkMode){
-    document.documentElement.style.setProperty("--background-colour", "#1E1E1E")
-  }
-  else{
-    document.documentElement.style.setProperty("--background-colour", "#FFFFFF")
+  var thisDict = darkMode ? darkModeColourDict : lightModeColourDict
+  for (var colourType in thisDict){
+    document.documentElement.style.setProperty(colourType, thisDict[colourType])
   }
 }
