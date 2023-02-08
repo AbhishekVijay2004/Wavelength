3e66924665d61048dcf7138e27af78dba85e5c00
Zak <zak.mitchell@student.manchester.ac.uk>
2023-02-08 10:30:12 +0000

    Backend testing finalisation.

--- Site/js/backend-test.js
+++ Site/js/backend-test.js
@@ -2,28 +2,28 @@ function backendTest(){
   alert("Working.");
 }
 var xhr = null;
-  getXmlHttpRequestObject = function () {
-      if (!xhr) {
-          // Create a new XMLHttpRequest object
-          xhr = new XMLHttpRequest();
-      }
-      return xhr;
-  };
-  function dataCallback() {
-      // Check response is ready or not
-      if (xhr.readyState == 4 && xhr.status == 200) {
-          console.log("Info received!");
-          dataDiv = document.getElementById('result-container');
-          // Set current data text
-          dataDiv.innerHTML = xhr.responseText;
-      }
-  }
-  function getInfo() {
-      console.log("Get info...");
-      xhr = getXmlHttpRequestObject();
-      xhr.onreadystatechange = dataCallback;
-      // asynchronous requests
-      xhr.open("GET", "http://localhost:6969/users", true);
-      // Send the request over the network
-      xhr.send(null);
-  }
+getXmlHttpRequestObject = function () {
+    if (!xhr) {
+        // Create a new XMLHttpRequest object
+        xhr = new XMLHttpRequest();
+    }
+    return xhr;
+};
+function dataCallback() {
+    // Check response is ready or not
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        console.log("Info received!");
+        dataDiv = document.getElementById('result-container');
+        // Set current data text
+        dataDiv.innerHTML = xhr.responseText;
+    }
+}
+function getInfo() {
+    console.log("Get info...");
+    xhr = getXmlHttpRequestObject();
+    xhr.onreadystatechange = dataCallback;
+    // asynchronous requests
+    xhr.open("GET", "http://localhost:80/jsontesting.json", true);
+    // Send the request over the network
+    xhr.send(null);
+}
