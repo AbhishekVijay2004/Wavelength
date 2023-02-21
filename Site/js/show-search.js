50f8daa57cabef0faadd143fb5513837007926a3
Zak <zak.mitchell@student.manchester.ac.uk>
2023-02-21 12:43:23 +0000

    Search result changes.

--- Site/js/show-search.js
+++ Site/js/show-search.js
@@ -1,11 +1,11 @@
 function ShowSearch(){
   var searchContainer = document.getElementById("search-container");
-  searchContainer.style.boxShadow = "0vw 0vw 2vw 0.5vw rgba(0, 0, 0, 0.5)";
+  //searchContainer.style.boxShadow = "0vw 0vw 2vw 0.5vw rgba(0, 0, 0, 0.5)";
   searchContainer.style.height = "62vh";
 }
 
 function HideSearch(){
   var searchContainer = document.getElementById("search-container");
-  searchContainer.style.boxShadow = "none";
+  //searchContainer.style.boxShadow = "none";
   searchContainer.style.height = "0";
 }
