db0ec5b6eff17629ec126612f1fada76c1b6dbd2
u38792zm <zak.mitchell@student.manchester.ac.uk>
2023-02-21 11:36:42 +0000

    Began adding search results container.

---
+++ Site/js/show-search.js
@@ -0,0 +1,7 @@
+function ShowSearch(){
+  document.getElementById("search-container").style.height = "62vh"
+}
+
+function HideSearch(){
+  document.getElementById("search-container").style.height = "0vh";
+}
