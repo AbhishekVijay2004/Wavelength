1e5cf25b5fc02516014152b99252a9db6950754e
u38792zm <zak.mitchell@student.manchester.ac.uk>
2023-03-07 12:24:11 +0000

    Search debugging

--- Site/static/js/search.js
+++ Site/static/js/search.js
@@ -1,6 +1,6 @@
 function Search(){
   var query = document.getElementById("search-box").value;
-  window.location.href = "{{url_for('search_song')}}";
+  window.location.href = "{{url_for('search_song(" + query + ")')}}";
   var NUM_RESULTS = 5;
   for (var i = 0; i < NUM_RESULTS; i++){
     var indexString = i.toString();
