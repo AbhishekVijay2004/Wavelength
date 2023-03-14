bef4aec7b12054fc3c5ebe6a905a2b55c913c232
r01479mo <matthew.ofarrelly@manchester.ac.uk>
2023-03-14 12:26:05 +0000

    fixing users

--- Site/dbfunctions.py
+++ Site/dbfunctions.py
@@ -13,7 +13,7 @@ def connectdb():
 
 
 
-def create_user(cursor, db, username, password, profilePic=None, email=None, bio=None, topsong=None, displayname=None):
+def create_user(cursor, db, username, password, email, profilePic=None, bio=None, topsong=None, displayname=None):
 	if displayname == None:
 		displayname = username
 	sql = """
@@ -148,6 +148,8 @@ def get_user_detail(cursor, db, username, param='*'):
 			WHERE (username = %s)"""
 		cursor.execute(sql, (username, ))
 		result = cursor.fetchone()
+		if len(result) == 0:
+			return None
 		return result
 
 def get_user_details(cursor, username):
@@ -156,6 +158,8 @@ def get_user_details(cursor, username):
 		WHERE (username = %s)"""
 	cursor.execute(sql, (username, ))
 	result = cursor.fetchall()
+	if len(result) == 0:
+		return None
 	return result[0]
 
 def get_user_details_by_email(cusor, email):
@@ -164,6 +168,8 @@ def get_user_details_by_email(cusor, email):
 		WHERE (email = %s)"""
 	cursor.execute(sql, (email,))
 	resultt = cursor.fetchall()
+	if len(result) == 0:
+		return None
 	return result[0]
 
 def get_post_details(cursor, db, postid, param='*',):
@@ -258,6 +264,6 @@ if __name__ == "__main__":
 	# create a user
 	# create_user(cursor, db, 'matt', 'password', 'matt@uni.init')
 	# create_post(cursor, db, 'matt', 'hello')
-	# add_comment(1, 'matt', 'this is shit', cursor, db)
-	# add_comment_like(1, 'matt', 'dislike', cursor, db)
+	# add_comment(2, 'matt', 'this is shit', cursor, db)
+	# add_comment_like(3, 'matt', 'dislike', cursor, db)
 	print(get_user_details(cursor, 'matt'))
