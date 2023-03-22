9b73bd88c4b398818baffb64fd6148f1fe374a9e
Zak <zak.mitchell@student.manchester.ac.uk>
2023-03-22 10:45:47 +0000

    Improve notifications.

--- Site/static/js/get-notifications.js
+++ Site/static/js/get-notifications.js
@@ -4,7 +4,7 @@ function GetNotifications(){
     type: 'GET',
     success: function(data) {
       var newHTML = "<h2 class='notifications-title'>Notifications</h2>";
-      for (var i = 0; i < data.length; i++){
+      for (var i = data.length - 1; i >= 0; i--){
         if (data[i].type == "Follow Request"){
           newHTML += "<div class='notification-box id" + data[i].notificationID + "' id='notification-" + i.toString() + "' style='height:13vw;'><h3 class='notification-type'>" + data[i].type + "<img class='notification-close' onclick='CloseNotification(\"" + i.toString() + "\")'></h3><img class='notification-picture' src='" + data[i].senderPic + "'><p class='notification-name'>" + data[i].sender + "</p><div class='follow-accept-button' onclick='AcceptRequest(\"" + data[i].sender + "\",\"" + i.toString() + "\")'>Accept</div></div>";
         }
@@ -39,3 +39,5 @@ function CloseNotification(notificationNum){
 function AcceptRequest(username, notificationNum){
   CloseNotification(notificationNum);
 }
+
+GetNotifications();
