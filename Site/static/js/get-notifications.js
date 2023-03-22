15fadf9ae9b5c3ccb57ead33298d4f67c1619129
Zak <zak.mitchell@student.manchester.ac.uk>
2023-03-22 10:34:14 +0000

    Notification periodic checker

--- Site/static/js/get-notifications.js
+++ Site/static/js/get-notifications.js
@@ -1,22 +1,25 @@
-$.ajax({
-  url: '/fetchNotification',
-  type: 'GET',
-  success: function(data) {
-    var newHTML = "<h2 class='notifications-title'>Notifications</h2>";
-    for (var i = 0; i < data.length; i++){
-      if (data[i].type == "Follow Request"){
-        newHTML += "<div class='notification-box id" + data[i].notificationID + "' id='notification-" + i.toString() + "' style='height:13vw;'><h3 class='notification-type'>" + data[i].type + "<img class='notification-close' onclick='CloseNotification(\"" + i.toString() + "\")'></h3><img class='notification-picture' src='" + data[i].senderPic + "'><p class='notification-name'>" + data[i].sender + "</p><div class='follow-accept-button' onclick='AcceptRequest(\"" + data[i].sender + "\",\"" + i.toString() + "\")'>Accept</div></div>";
+function GetNotifications(){
+  $.ajax({
+    url: '/fetchNotification',
+    type: 'GET',
+    success: function(data) {
+      var newHTML = "<h2 class='notifications-title'>Notifications</h2>";
+      for (var i = 0; i < data.length; i++){
+        if (data[i].type == "Follow Request"){
+          newHTML += "<div class='notification-box id" + data[i].notificationID + "' id='notification-" + i.toString() + "' style='height:13vw;'><h3 class='notification-type'>" + data[i].type + "<img class='notification-close' onclick='CloseNotification(\"" + i.toString() + "\")'></h3><img class='notification-picture' src='" + data[i].senderPic + "'><p class='notification-name'>" + data[i].sender + "</p><div class='follow-accept-button' onclick='AcceptRequest(\"" + data[i].sender + "\",\"" + i.toString() + "\")'>Accept</div></div>";
+        }
+        else{
+          newHTML += "<div class='notification-box id" + data[i].notificationID + "' id='notification-" + i.toString() + "'><h3 class='notification-type'>" + data[i].type + "<img class='notification-close' onclick='CloseNotification(\"" + i.toString() + "\")'></h3><img class='notification-picture' src='" + data[i].senderPic + "'><p class='notification-name'>" + data[i].sender + "</p></div>";
+        }
       }
-      else{
-        newHTML += "<div class='notification-box id" + data[i].notificationID + "' id='notification-" + i.toString() + "'><h3 class='notification-type'>" + data[i].type + "<img class='notification-close' onclick='CloseNotification(\"" + i.toString() + "\")'></h3><img class='notification-picture' src='" + data[i].senderPic + "'><p class='notification-name'>" + data[i].sender + "</p></div>";
-      }
+      document.getElementById("notifications-container").innerHTML = newHTML;
+      setTimeout(GetNotifications, 1000);
+    },
+    error: function(error) {
+    console.error(error);
     }
-    document.getElementById("notifications-container").innerHTML = newHTML;
-  },
-  error: function(error) {
-  console.error(error);
-  }
-});
+  });
+}
 
 function CloseNotification(notificationNum){
   var elem = document.getElementById("notification-" + notificationNum);
